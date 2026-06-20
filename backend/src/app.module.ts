87f8ab40ef52348b6a2cf37ea28b1f3e9f9bad0a
Wallace Monsores <wallacemonsoress@gmail.com>
2026-06-20 20:09:23 -0300

    Register operational modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { CargosModule } from './cargos/cargos.module';
 import { ChecklistsModule } from './checklists/checklists.module';
+import { ExecucoesModule } from './execucoes/execucoes.module';
+import { FotosModule } from './fotos/fotos.module';
 import { LojasModule } from './lojas/lojas.module';
+import { MeModule } from './me/me.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TurnosModule } from './turnos/turnos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
@@ -18,6 +22,10 @@ import { UsuariosModule } from './usuarios/usuarios.module';
     TurnosModule,
     UsuariosModule,
     ChecklistsModule,
+    ExecucoesModule,
+    MeModule,
+    AdminModule,
+    FotosModule,
   ],
 })
 export class AppModule {}
