47c01a6b19fc85c7f7c5ffb7f4a7dbfecfdc38e5
Wallace Monsores <wallacemonsoress@gmail.com>
2026-06-20 19:10:43 -0300

    Add roles guard

---
+++ backend/src/common/guards/roles.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { TipoUsuario } from '@prisma/client';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<TipoUsuario[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || !requiredRoles.includes(user.tipoUsuario)) {
+      throw new ForbiddenException('Acesso negado.');
+    }
+
+    return true;
+  }
+}
