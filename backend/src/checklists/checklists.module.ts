fc972e73e6e867357b26103e7fc0e0fa310cac79
Wallace Monsores <wallacemonsoress@gmail.com>
2026-06-20 20:09:41 -0300

    Register checklist helper services

--- backend/src/checklists/checklists.module.ts
+++ backend/src/checklists/checklists.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ChecklistsController } from './checklists.controller';
 import { ChecklistTarefasService } from './services/checklist-tarefas.service';
+import { ChecklistValidationsService } from './services/checklist-validations.service';
+import { ChecklistsQueryService } from './services/checklists-query.service';
 import { ChecklistsService } from './services/checklists.service';
 
 @Module({
   controllers: [ChecklistsController],
-  providers: [ChecklistsService, ChecklistTarefasService],
+  providers: [
+    ChecklistsService,
+    ChecklistsQueryService,
+    ChecklistValidationsService,
+    ChecklistTarefasService,
+  ],
 })
 export class ChecklistsModule {}
